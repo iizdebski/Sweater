3eaf47265be555cc79efb779c1ab081c740524fd
iizdebski <39988554+iizdebski@users.noreply.github.com>
2020-08-06 11:06:24 +0300

    17_springboot_integration_testing_spring_testing

--- src/main/java/com/izdebski/sweater/Application.java
+++ src/main/java/com/izdebski/sweater/Application.java
@@ -2,6 +2,7 @@ package com.izdebski.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class Application {
